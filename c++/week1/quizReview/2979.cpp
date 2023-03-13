a8343daffe7f3b278286a5261d67ae166abb8576
0131ryu <131ryuji@gmail.com>
2023-03-13 16:18:18 +0900

    2309, 2979, 10808 복습

---
+++ c++/week1/quizReview/2979.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdioc++.h>
+using namespace std;
+int A, B, C, a, b, ret;
+int main()
+{
+    cin >> A >> B >> C;
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a >> b;
+    }
+    return 0;
+}
