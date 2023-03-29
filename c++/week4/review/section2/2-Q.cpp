5ef1f64c55347bfb33af4051e54245e82a8b2142
0131ryu <131ryuji@gmail.com>
2023-03-29 18:24:47 +0900

    1-G, 1-H, 2-Q, 2-R

---
+++ c++/week4/review/section2/2-Q.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
+int n, m, a[104][104], visited[104][104], cnt, cnt2;
+vector<pair<int, int>> v;
+void go(int y, int x)
+{
+    visited[y][x] = 1;
+    if (a[y][x] == 1)
+    {
+        v.push_back({y, x});
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int ny = dy[i] + y;
+        int nx = dx[i] + x;
+        if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx])
+            continue;
+        go(ny, nx);
+    }
+    return;
+}
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    while (true)
+    {
+        cnt2 = 0;
+        fill(&visited[0][0], &visited[0][0] + 104 * 104, 0);
+        v.clear();
+        go(0, 0);
+        for (pair<int, int> b : v)
+        {
+            cnt2++;
+            a[b.first][b.second] = 0; // 0이 아니면 루프 내부
+        }
+        bool flag = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (a[i][j] != 0)
+                    flag = 1; // 0이 아닌 경우 flag는 true로 값이 0이면 배열의 나머지 요소 계속 확인
+            }
+        }
+        cnt++;
+        if (!flag)
+            break;
+    }
+    cout << cnt << '\n'
+         << cnt2 << '\n';
+}
