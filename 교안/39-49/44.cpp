9ba12a1e1908d38c8124d0470d0f9fd5591e24d7
0131ryu <131ryuji@gmail.com>
2023-05-30 15:53:12 +0900

    교안 39 ~ 49

---
+++ 교안/39-49/44.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 2147483647;
+    int b = -2147483648;
+    cout << a << '\n';
+    cout << b << '\n';
+    a++;
+    b--;
+    cout << a << '\n';
+    cout << b << '\n';
+}
---
+++ 교안/39-49/49.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a = 'a';
+    char c = 'c';
+    char d = 'd';
+    char z = 'z';
+    cout << (int)a << " : " << (int)c << " : "
+         << (int)d << " : " << (int)z << '\n';
+    cout << (int)a - 97 << '\n';
+    cout << (int)a - 'a' << '\n';
+    cout << (int)c - 99 << '\n';
+    cout << (int)c - 'c' << '\n';
+    cout << (int)d - 100 << '\n';
+    cout << (int)d - 'd' << '\n';
+    cout << (int)z - 122 << '\n';
+    cout << (int)z - 'z' << '\n';
+    return 0;
+}
